f58412f3cae96ff49ba489a4ed66de5fa9bdb9aa
Cledio17 <clementd0317@gmail.com>
2023-10-10 17:58:55 +0800

    Add project files.

---
+++ BankWebAPI/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace BankWebAPI.Models
+{
+    public class Transaction
+    {
+        public string transId { get; set; }
+        public string fromId { get; set; }
+        public string toId { get; set; }
+        public double bal { get; set; }
+    }
+}
---
+++ BankWebAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace BankWebAPI.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string pfp { get; set; }
+        public string acctNo { get; set; }
+    }
+}
